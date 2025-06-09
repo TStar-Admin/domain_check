828419db0993a7900f9d5a96afee13bec7c6e74a
忽有狂徒夜磨刀 <57833202+TStar-Admin@users.noreply.github.com>
2025-06-09 19:01:19 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 echo "hello openwrt, $(date '+%Y-%m-%d %H:%M:%S')"
-MAC_ADDRESS=$(cat /sys/class/net/eth0/address)
 
+IFACE="eth0"
+MAC_ADDRESS=$(cat /sys/class/net/${IFACE}/address 2>/dev/null)
+
+if [ -z "$MAC_ADDRESS" ]; then
+  echo "❌ MAC 地址获取失败"
+  exit 1
+fi
 
 UPTIME_SEC=$(cut -d. -f1 /proc/uptime)
-response=$(wget -qO- "https://online.sharewifi.cc/api/InterfaceHeartBeat?mac=$MAC_ADDRESS&id=Google&time=$UPTIME_SEC")
-echo response;
+
+URL="https://online.sharewifi.cc/api/InterfaceHeartBeat?mac=$MAC_ADDRESS&id=Google&time=$UPTIME_SEC"
+echo "访问URL: $URL"
+
+response=$(wget -qO- "$URL")
+echo "服务器返回：$response"
