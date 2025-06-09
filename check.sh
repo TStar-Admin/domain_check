7db026483b3204c79439c8797bc6f69fce81e6ac
忽有狂徒夜磨刀 <57833202+TStar-Admin@users.noreply.github.com>
2025-06-09 18:55:58 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 echo "hello openwrt, $(date '+%Y-%m-%d %H:%M:%S')"
+MAC_ADDRESS=$(cat /sys/class/net/eth0/address)
+
+
+UPTIME_SEC=$(cut -d. -f1 /proc/uptime)
+response=$(wget -qO- "https://online.sharewifi.cc/api/InterfaceHeartBeat?mac=$MAC_ADDRESS&id=Google&time=$UPTIME_SEC")
+echo response;
